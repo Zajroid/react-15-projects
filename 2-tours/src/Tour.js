652dd7d722db9c6783bc6c3b41e206cc1852ce35
Danya <zajroid@gmail.com>
2022-08-26 15:50:16 +0300

    completed 2-tours

--- 2-tours/src/Tour.js
+++ 2-tours/src/Tour.js
@@ -1,8 +1,27 @@
 import React, {useState} from 'react';
 
-const Tour = () => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
-    <h2>Tour component</h2>
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : ' read more'}
+          </button>
+        </p>
+        <button className='delete-btn' onClick={() => removeTour(id)}>
+          not interesed
+        </button>
+      </footer>
+    </article>
   );
 }
 
